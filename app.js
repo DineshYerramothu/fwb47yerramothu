8756bdd9d69c73be59cff7ee7e3c27dc553d39de
Dinesh Yerramothu <dyerramothu@gmail.com>
2021-11-03 02:42:01 -0500

    Bonus done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var echoRouter = require('./routes/echo');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/echo', echoRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
